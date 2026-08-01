7d23701e28800a0dd376bb992ff1dc7d9c2053c9
udodi-n <itsudodi@gmail.com>
2026-08-01 21:27:53 +0100

    #4

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,89 @@
-function Header(){
-return (
-    <>
-    <header className="flex w-full p-4 items-center fixed justify-between bg-white">
-        <div className="flex items-end h-fit gap-2">
-            <img src="/zipicon_logo.png" className="h-8" alt="ZipIcon logo" />
-            <p className="text-2xl font-bold">Zipicon</p>
-        </div>
-        <a
-            href="https://github.com/udodi-n/zipicon"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="px-6 lg:flex lg:px-8 py-3 bg-black rounded-full text-white"
-        >
-            Star on GitHub
-        </a>
-    </header>
-    </>
-);
-}
+    import { useState, useEffect } from "react";
+    import axios from "axios";
+    import { nanoid } from "nanoid";
 
-export default Header
+    function Header(objects) {
+    const [selected, setSelected] = useState(false)
+    console.log(Object.keys(objects.objects));
+
+    async function sendContainer(e) {
+        e.preventDefault();
+        console.log("gotti");
+        axios
+        .get(`${import.meta.env.VITE_FETCH_URL || "http://localhost:3000/"}zip`, {
+            params: {
+            query: JSON.stringify(objects.objects),
+            },
+            responseType: "arraybuffer",
+        })
+        .then((res) => {
+            const id = nanoid(3);
+            const blob = new Blob([res.data], { type: "application/zip" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = `zipicons_${id}.zip`;
+            a.click();
+            URL.revokeObjectURL(url);
+        });
+    }
+    return (
+        <>
+        <header className="flex w-full p-4 z-999 items-center fixed justify-between bg-white">
+            <div className="flex items-end h-fit gap-2">
+            <img src="/zipicon_logo.png" className="h-6" alt="ZipIcon logo" />
+            <p className="text-xl font-bold">Zipicon</p>
+            </div>
+            <div className="flex items-center gap-6">
+            <div className="aspect-square h-7 w-7 rounded-full fill-[#E73F1E] flex justify-center items-center relative">
+                <div className={`absolute top-full  mt-9  flex-col justify-center items-center gap-2 ${selected ? "flex dropdown-combo" : "hidden"}`}>
+                <div className="h-fit max-h-[70vh] pt-10 w-[min(95vw,20rem)] px-2 pt-5 flex flex-col items-center overflow-y-auto noscroll rounded-2xl bg-white">
+                    {Object.keys(objects.objects).map((key) => {
+                    return (
+                        <div
+                        key={key}
+                        className="flex items-center w-full justify-between text-3xl font-light gap-2 p-2"
+                        >
+                        <div
+                            className="flex gap-2 aspect-square h-14 items-center p-2"
+                            dangerouslySetInnerHTML={{
+                            __html: objects.objects[key].data,
+                            }}
+                        ></div>
+                        <p>{objects.objects[key].name}</p>
+                        </div>
+                    );
+                    })}
+                </div>
+                <button
+                    onClick={sendContainer}
+                    className="mt-4 bg-[#E73F1E] text-white w-full py-4 text-xl gap-5 flex items-center justify-center gap-2 rounded-full"
+                >
+                    Zip <img className="invert" src="/folder_zip.svg" alt="" />
+                </button>
+                </div>
+                <svg
+                onClick={() => setSelected(!selected)}
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <title>Development Containers</title>
+                <path d="M10.31.615a4.5 4.5 0 0 1 3.382 0l8.998 3.648A2.1 2.1 0 0 1 24 6.208v11.584a2.1 2.1 0 0 1-1.311 1.946l-8.998 3.648a4.5 4.5 0 0 1-3.382 0l-8.998-3.648A2.1 2.1 0 0 1 0 17.792V6.208a2.1 2.1 0 0 1 1.311-1.946Zm2.705 1.668a2.7 2.7 0 0 0-2.028 0l-9 3.647a.3.3 0 0 0-.187.278v11.584a.3.3 0 0 0 .187.278l8.999 3.648a2.7 2.7 0 0 0 2.028 0l8.999-3.648a.3.3 0 0 0 .187-.278V6.208a.3.3 0 0 0-.187-.278ZM6.019 6.658 12 8.928l5.98-2.27c1.122-.427 1.762 1.256.64 1.683l-5.72 2.17V17.1c0 1.2-1.8 1.2-1.8 0v-6.59L5.38 8.34c-1.122-.426-.482-2.109.64-1.683" />
+                </svg>
+            </div>
+            <a
+                href="https://github.com/udodi-n/zipicon"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-6 lg:flex lg:px-8 py-3 bg-black rounded-full text-white"
+            >
+                Star on GitHub
+            </a>
+            </div>
+        </header>
+        </>
+    );
+    }
+
+    export default Header;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,233 +1,248 @@
-import { useState, useRef, useEffect } from "react";
-import { nanoid } from "nanoid";
-import axios from "axios"
-import Header from "./Header"
+    import { useState, useRef, useEffect } from "react";
+    import { nanoid } from "nanoid";
+    import axios from "axios"
+    import Header from "./Header"
 
-function Home() {
-const [text, setText] = useState("");
-const [results, setResults] = useState([]);
-const [selectedName, setSelectedName] = useState("")
-const [selectedIcon, setSelectedIcon] = useState("")
-const [selectState, setSelectState] = useState(false)
-async function fetchIcons(e) {
-e.preventDefault();
-const data = axios.get('http://localhost:3000/search', {
-params: {
-query: text
-}
-})
-.then((res) => {
-setResults(res.data);
-console.log(res.data)
-})
-.catch((err) => console.error(err))
-// const data = await fetch(
-// `${import.meta.env.VITE_FETCH_URL || "http://localhost:3000/"}api/sickons`,
-// {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ iconReq: text.replace(/\s/g, "").toLowerCase() }),
-// }
-// );
-// const id = nanoid(3);
-// const blob = await data.blob();
-// const url = URL.createObjectURL(blob);
-// const a = document.createElement("a");
-// a.href = url;
-// a.download = `zipicons_${id}.zip`;
-// a.click();
-// URL.revokeObjectURL(url);
-}
+    function Home() {
+    const containerSet = {}
+    const [text, setText] = useState("");
+    const [results, setResults] = useState([]);
+    const [selectedName, setSelectedName] = useState("")
+    const [selectedIcon, setSelectedIcon] = useState("")
+    const [selectState, setSelectState] = useState(false)
+    const [container, setContainer] = useState({})
+    async function fetchIcons(e) {
+    e.preventDefault();
+    const data = axios.get(`${import.meta.env.VITE_FETCH_URL || "http://localhost:3000/"}search`, {
+    params: {
+        query: text,
+    },
+    })
+    .then((res) => {
+    setResults(res.data);
+    console.log(res.data);
+    })
+    .catch((err) => console.error(err));
+    }
+    const cont = JSON.parse(localStorage.getItem("container"))
+    function addtoContainer(e){
+        e.preventDefault()
+        const existence = localStorage.getItem("container");
+        existence ? (
+            cont[selectedName] = { name: selectedName, data: selectedIcon },
+            localStorage.setItem("container", JSON.stringify(cont))
 
-useEffect(() => {
-function displayIcons(){
-console.log('hella')
-const data = axios.get("http://localhost:3000/search", {
-params: {
-    query: "arr",
-},
-})
-.then((res) => {
-setResults(res.data);
-console.log(res.data);
-})
-.catch((err) => console.error(err));
-}
+        ) : (
+        containerSet[selectedName] = {name: selectedName, data: selectedIcon},
+        localStorage.setItem("container", JSON.stringify(containerSet))
+        )
+    }
 
-return () => displayIcons()
-}, [])
+    function downloadSVG(e){
+        e.preventDefault()
+        const svgString = selectedIcon;
+        const blob = new Blob([svgString], { type: "image/svg+xml;charset=utf-8" });
+        const url = URL.createObjectURL(blob);
+        const downloadLink = document.createElement("a");
+        downloadLink.href = url;
+        downloadLink.download = `${selectedName}.svg`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+        URL.revokeObjectURL(url);
+    }
 
-return (
-<>
-<div className="font-[SF_UI_Display] flex flex-col">
-<main className="h-screen w-full flex flex-col text-sm">
-<Header />
-<section
-className="gap-12 flex flex-col justify-center flex-1"
-id="hero"
-style={{
-backgroundImage: "url(/bg_2.jpg)",
-}}
->
-<div className="w-full h-full bg-black/50 p-8 gap-12 px-5 flex flex-col justify-end flex-1">
-<div className="flex flex-col gap-6 w-fit lg:pb-20">
-    <h1
-    className="text-5xl text-white lg:text-8xl text-left lg:leading-20"
-    style={{ position: "relative" }}
-    >
-    For lazy devs that don't <br /> use icon libraries
-    </h1>
-    <p className="text-lg text-gray-300 font-light">
-    UNDER CONSTRUCTION
-    </p>
-    {/* <p className="text-lg text-gray-300 font-light">
-    Find the perfect icon for your project in seconds. Zip it and
-    Ship it!
-    </p> */}
-    <div className=" font-light h-15 w-full flex gap-6 justify-center lg:justify-start items-center">
-    <div className="h-full flex w-fit justify-between items-center gap-4">
-        {/* div wrapper */}
-        <div className="max-w-7 fill-white">
-        <svg
-            role="img"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <title>GitHub</title>
-            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-        </svg>
-        </div>
-        <p className="text-white">Star on GitHub</p>
-    </div>
-    <div className="h-full px-5 w-45 bg-black invert rounded-full  flex justify-center gap-6 items-center gap-3">
-        {/* div wrapper */}
-        <div className="max-w-7 invert">
-        <svg
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path d="M10.4883 14.651L15.25 21H22.25L14.3917 10.5223L20.9308 3H18.2808L13.1643 8.88578L8.75 3H1.75L9.26086 13.0145L2.31915 21H4.96917L10.4883 14.651ZM16.25 19L5.75 5H7.75L18.25 19H16.25Z" />
-        </svg>
-        </div>
-        <p className="text-white">Follow on X</p>
-    </div>
-    </div>
-</div>
-</div>
-</section>
-</main>
-{/* form start */}
-<section className="flex flex-col px-6 gap-8">
-<form
-    action=""
-    style={{ boxShadow: "0px 3px 4px #0000001e" }}
-    className="flex w-full rounded-full mt-15 h-12 lg:w-full py-2 px-2 border-[#cccccc] border-1"
-    onSubmit={fetchIcons}
->
-    {/* search button start */}
-    <div className="flex h-full items-end">
-    <button
-        className="flex justify-center items-center rounded-full aspect-square h-full"
-        type="submit"
-    >
-        <svg
-        className="lucide opacity-60 lucide-search"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        >
-        <path d="m21 21-4.34-4.34" />
-        <circle cx="11" cy="11" r="8" />
-        </svg>
+    useEffect(() => {
+    function displayIcons(){
+    const cont = JSON.parse(localStorage.getItem("container"))
+    setContainer(cont)
+    console.log('hella')
+    const data = axios.get(`${import.meta.env.VITE_FETCH_URL || "http://localhost:3000/"}search`, {
+    params: {
+        query: "arr",
+    },
+    })
+    .then((res) => {
+    setResults(res.data);
+    console.log(res.data);
+    })
+    .catch((err) => console.error(err));
+    }
 
-    </button>
-    </div>
-    {/* search buttoon end */}
+    return () => displayIcons()
+    }, [])
 
-    {/* the search input */}
-    <input
-    className="px-5 pt flex flex-1 font-light focus:outline-none"
-    placeholder="google, facebook, instagram"
-    onChange={(e) => setText(e.target.value)}
-    value={text}
-    type="text"
-    />
-    {/* search input ends here */}
-</form>
-{/* form end */}
-<div
-style={{
-    gridAutoColumns: "minmax(0, 1fr)",
-    gridTemplateColumns: "repeat(auto-fill, minmax(5rem, 1fr))",
-    display: "grid",
-    alignItems: "top",
-}}
->
-{results.map((result) => {
     return (
-        <div key={result.name} className="rounded-md">
-        <div
-        className=""
-            style={{
-            aspectRatio: "1/1",
-            width: "calc(6rem + 1vh)",
-            placeItems: "center",
-            
-            placeContent: "center",
-            }}
+    <>
+    <div className="font-[SF_UI_Display] flex flex-col">
+    <main className="h-screen w-full flex flex-col text-sm">
+    <Header objects={container}/>
+    <section
+    className="gap-12 flex flex-col justify-center flex-1"
+    id="hero"
+    style={{
+    backgroundImage: "url(/bg_2.jpg)",
+    }}
+    >
+    <div className="w-full h-full bg-black/50 p-8 gap-12 px-5 flex flex-col justify-end flex-1">
+    <div className="flex flex-col gap-6 w-fit lg:pb-20">
+        <h1
+        className="text-5xl text-white lg:text-8xl text-left lg:leading-20"
+        style={{ position: "relative" }}
         >
-            <div
-            onClick={() => {setSelectedName(result.name); setSelectedIcon(result.svg); setSelectState(true)}}
-            className="h-3/10 hover:scale-110 transition-all cursor-pointer w-3/10 flex justify-center items-center"
-            dangerouslySetInnerHTML={{ __html: result.svg }}
-            />
-            <p className="text-gray-500 mt-2 text-sm font-light">{result.name.slice(0, 7) + "..."}</p>
-        </div>
-        </div>
-    );
-})}
-</div>
-</section>
-<div  style={{ boxShadow: "0px 3px 5px #0000001e" }} className= {`lg:aspect-7/4 aspect-6/4 bg-white border-1 border-[#cccccc] rounded-2xl h-60 lg:h-60 bottom-0 right-0 mr-5 mb-5 overflow-hidden fixed ${selectState? 'block' : 'hidden'}`}>
-<div className="flex flex-col h-full w-full">
-    <div id="header" className="h-10 bg-[#7E1DFB] w-full flex justify-end items-center gap-2 px-4">
-        <div onClick={() => setSelectState(false)} className="h-1 w-5 bg-white"></div>
-    </div>
-    <div id="rest" className=" flex flex-1 flex-col h-full">
-        <div className="flex h-full w-full" id="top">
-            <div className="left p-10 " dangerouslySetInnerHTML={{ __html: selectedIcon }}>
+        For lazy devs that don't <br /> use icon libraries
+        </h1>
+        {/* <p className="text-lg text-gray-300 font-light">
+        UNDER CONSTRUCTION
+        </p> */}
+        <p className="text-lg text-gray-300 font-light">
+        Find the perfect icon for your project in seconds. Zip it and
+        Ship it!
+        </p>
+        <div className=" font-light h-15 w-full flex gap-6 justify-start items-center">
+        <div className="h-full flex w-fit justify-between items-center gap-4">
+            {/* div wrapper */}
+            <div className="max-w-7 fill-white">
+            <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <title>GitHub</title>
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+            </svg>
             </div>
-            <div className="right  flex flex-col justify-center h-full w-full">
-                <div>
-                    <p className="font-bold">Name:</p>
-                    <p className="font-light">{selectedName}</p>
-                </div>
-                <div>
-                    <p className="font-bold">Marital Status:</p>
-                    <p className="font-light">{(() => {
-                        const status = ["single", "married", "divorced", "widowed", "crazy", "trying"];
-                        return <p>{status[Math.floor(Math.random() * status.length)]}</p>;
-                    })()}</p>
-                </div>
-                <div>
-                    <p className="font-bold">Actions:</p>
-                    <p className="font-light"><a href="#" className="underline">add to container</a> / <a href="#" className="hover:underline">download</a></p>
-                </div>
+            <p className="text-white">Star on GitHub</p>
+        </div>
+        <div className="h-4/5 px-5 w-45 bg-[#E73F1E]  rounded-full flex justify-center gap-4 items-center">
+            {/* div wrapper */}
+            <div className="max-w-7 invert">
+            <svg
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path d="M10.4883 14.651L15.25 21H22.25L14.3917 10.5223L20.9308 3H18.2808L13.1643 8.88578L8.75 3H1.75L9.26086 13.0145L2.31915 21H4.96917L10.4883 14.651ZM16.25 19L5.75 5H7.75L18.25 19H16.25Z" />
+            </svg>
             </div>
+            <p className="text-white">Follow on X</p>
+        </div>
         </div>
     </div>
-</div>
-</div>
-</div>
-</>
-);
-}
+    </div>
+    </section>
+    </main>
+    {/* form start */}
+    <section className="flex flex-col px-6 gap-8">
+    <form
+        action=""
+        style={{ boxShadow: "0px 3px 4px #0000001e" }}
+        className="flex w-full rounded-full mt-15 h-12 lg:w-full py-2 px-2 border-[#cccccc] border-1"
+        onSubmit={fetchIcons}
+    >
+        {/* search button start */}
+        <div className="flex h-full items-end">
+        <button
+            className="flex justify-center items-center rounded-full aspect-square h-full"
+            type="submit"
+        >
+            <svg
+            className="lucide opacity-60 lucide-search"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            >
+            <path d="m21 21-4.34-4.34" />
+            <circle cx="11" cy="11" r="8" />
+            </svg>
 
-export default Home;
+        </button>
+        </div>
+        {/* search buttoon end */}
+
+        {/* the search input */}
+        <input
+        className="px-5 pt flex flex-1 font-light focus:outline-none"
+        placeholder="google, facebook, instagram"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        type="text"
+        />
+        {/* search input ends here */}
+    </form>
+    {/* form end */}
+    <div
+    style={{
+        gridAutoColumns: "minmax(0, 1fr)",
+        gridTemplateColumns: "repeat(auto-fill, minmax(5rem, 1fr))",
+        display: "grid",
+        alignItems: "top",
+    }}
+    >
+    {results.map((result) => {
+        return (
+            <div key={result.name} className="rounded-md">
+            <div
+            className=""
+                style={{
+                aspectRatio: "1/1",
+                width: "calc(6rem + 1vh)",
+                placeItems: "center",
+                
+                placeContent: "center",
+                }}
+            >
+                <div
+                onClick={() => {setSelectedName(result.name); setSelectedIcon(result.svg); setSelectState(true)}}
+                className="h-3/10 hover:scale-110 transition-all cursor-pointer w-3/10 flex justify-center items-center"
+                dangerouslySetInnerHTML={{ __html: result.svg }}
+                />
+                <p className="text-gray-500 mt-2 text-sm font-light">{result.name.slice(0, 7) + "..."}</p>
+            </div>
+            </div>
+        );
+    })}
+    </div>
+    </section>
+    <div  style={{ boxShadow: "0px 3px 5px #0000001e" }} className= {`lg:aspect-7/4 aspect-6/4 bg-white border-1 border-[#cccccc] rounded-2xl h-60 lg:h-60 bottom-0 right-0 mr-5 mb-5 overflow-hidden fixed card-element ${selectState ? 'opacity-100 scale-100 pointer-events-auto' : 'opacity-0 scale-0 pointer-events-none'}`}>
+    <div className="flex flex-col h-full w-full">
+        <div id="header" className="h-10 bg-[#E73F1E] w-full flex justify-end items-center gap-2 px-4">
+            <div onClick={() => setSelectState(false)} className="h-1 w-5 bg-white"></div>
+        </div>
+        <div id="rest" className=" flex flex-1 flex-col h-full">
+            <div className="flex h-full w-full" id="top">
+                <div className="left p-10 " dangerouslySetInnerHTML={{ __html: selectedIcon }}>
+                </div>
+                <div className="right  flex flex-col justify-center h-full w-full">
+                    <div>
+                        <p className="font-bold">Name:</p>
+                        <p className="font-light">{selectedName}</p>
+                    </div>
+                    <div>
+                        <p className="font-bold">Marital Status:</p>
+                        <p className="font-light">{(() => {
+                            const status = ["single", "married", "divorced", "widowed", "crazy", "trying"];
+                            return <p>{status[Math.floor(Math.random() * status.length)]}</p>;
+                        })()}</p>
+                    </div>
+                    <div>
+                        <p className="font-bold">Actions:</p>
+                        <p className="font-light"><a href="" onClick={addtoContainer} className="underline">add to container</a> / <a href="" onClick={downloadSVG} className="hover:underline">download</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </>
+    );
+    }
+
+    export default Home;
